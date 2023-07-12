e6ca0d28ffa8a84c18d302d3b73b1b52692cc939
hedrall <masaharu73@gmail.com>
2023-07-13 07:04:27 +0900

    setup deploy action

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mermaid-quadrant-chart-ui-builder/',
   plugins: [react()],
-  build: {
-    // outDir: 'doc',
-  },
 });
